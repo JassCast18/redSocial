7693c401d6f71eda3a915dc2c4ba46e089eec72d
JassCast18 <jasscast18@gmail.com>
2025-05-12 08:57:19 -0600

    Agregado del Front

--- src/db.js
+++ src/db.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
     try {
-      await mongoose.connect("mongodb://localhost/pruebaproye");
+      //await mongoose.connect("mongodb://localhost/pruebaproye");
+      await mongoose.connect("mongodb+srv://rcastellanosl:HgeHcwUuONiYMZYy@proyecto.cikwcwk.mongodb.net/?retryWrites=true&w=majority&appName=Proyecto");
         console.log(">>> db is connected");
     } catch (error) {
         console.log('Error connecting to MongoDB:', error);
--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -3,19 +3,7 @@ export const validateSchema = (schema) => (req, res, next) => {
         schema.parse(req.body);
         next();
     } catch (error) {
-        if (error.errors) { // Para versiones antiguas de Zod
-            return res.status(400).json({
-                error: error.errors.map((err) => err.message)
-            });
-        } else if (error.issues) { // Para versiones recientes de Zod
-            return res.status(400).json({
-                error: error.issues.map((issue) => {
-                    // Prioriza el mensaje personalizado, luego el código
-                    return issue.message || `${issue.path.join('.')} - ${issue.code}`;
-                })
-            });
-        } else {
-            return res.status(400).json({ error: ["Error de validación"] });
-        }
+        return res.status(400)
+        .json({error : error.errors.map((error) =>error.message)})
     }
 };
