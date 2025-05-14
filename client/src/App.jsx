17c15af4de200dd0bfa7d94569c578bfef531d27
JassCast18 <jasscast18@gmail.com>
2025-05-13 19:58:54 -0600

    Cambios del Front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 
 function App(){
   return(
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home Page</h1>}/>
+        <Route path='/login' element={<h1>Login</h1>}/>
+        <Route path='/register' element={<h1>Register</h1>}/>
+        <Route path='/Publicaciones' element={<h1>Publicaciones</h1>}/>
+        <Route path='/add-publicacion' element={<h1>new publication</h1>}/>
+        <Route path='/profile' element={<h1>profile</h1>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
