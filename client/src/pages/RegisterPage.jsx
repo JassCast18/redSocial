57309c64868dd6118ae38c466e07f255df95fe40
JassCast18 <125106510+JassCast18@users.noreply.github.com>
2025-05-17 20:13:13 -0600

    avance

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,15 +1,17 @@
-import {useForm} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { userAuth } from '../context/AuthContext';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useCloudinary } from '../context/cloudinary';
 
 function RegisterPage() {
 
-    const {register, handleSubmit, formState:{
+    const { register, handleSubmit, formState: {
         errors
-    }} = useForm();
-    const {signup, isAuthenticated} = userAuth();
+    } } = useForm();
+    const { signup, isAuthenticated } = userAuth();
     const navigation = useNavigate();
+    const { image, loading, uploadImage } = useCloudinary();
 
     useEffect(() => {
         if (isAuthenticated) navigation('/profile');
@@ -19,47 +21,54 @@ function RegisterPage() {
         signup(values);
     });
 
-  return (
-    <div className="bg-zinc-800 max-w-md p-10 roudend-md">
-        <form onSubmit={onSubmit}>
-            <input type="text" 
-                {... register("username",{required:true})}
-                className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-                placeholder='username'
-            />
-            <input type="email" 
-                {... register("email",{required: true})}
-                className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-                placeholder='email'
-            />
-            <input type="password" 
-                {... register("password", {required: true})}
-                className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-                placeholder='password'
-            />
-            <input type="text" 
-                {... register("nombre", {required: true})}
-                className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-                placeholder='nombre'
-            />
-             <input type="text" 
-                {... register("Apellido", {required: true})}
-                className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-                placeholder='Apellido'
-            />
-            <input type="date" 
-                {... register("fechaNacimiento", {required: true})}
-                className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-                placeholder='fechaNacimiento'
-            />
-            <button type="submit">
-                Register
-            </button>
-        </form>
+    return (
+        <div className="bg-zinc-800 max-w-md p-10 roudend-md">
+            <form onSubmit={onSubmit}>
+                <input type="text"
+                    {...register("username", { required: true })}
+                    className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                    placeholder='username'
+                />
+                <input type="email"
+                    {...register("email", { required: true })}
+                    className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                    placeholder='email'
+                />
+                <input type="password"
+                    {...register("password", { required: true })}
+                    className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                    placeholder='password'
+                />
+                <input type="text"
+                    {...register("nombre", { required: true })}
+                    className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                    placeholder='nombre'
+                />
+                <input type="text"
+                    {...register("Apellido", { required: true })}
+                    className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                    placeholder='Apellido'
+                />
+                <input type="date"
+                    {...register("fechaNacimiento", { required: true })}
+                    className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                    placeholder='fechaNacimiento'
+                />
+                <div>
+                    <input type="file" onChange={uploadImage} />
+
+                    {loading && <p>Loading...</p>}
+                    {image && <img src={image} alt="Uploaded" style={{ width: '200px' }} />}
+                </div>
+
+                <button type="submit">
+                    Register
+                </button>
+            </form>
 
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default RegisterPage
