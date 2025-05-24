315b15e782e639112831a715f761b8d52e27c736
susanixcamparij <124958010+susanixcamparij@users.noreply.github.com>
2025-05-23 21:30:49 -0600

    cambio en Authcontext

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,41 +1,57 @@
 import { createContext, useState, useContext } from "react";
-import {registerRequest} from "../api/auth";
+import { registerRequest, loginRequest } from "../api/auth";
 
+export const AuthContext = createContext();
 
-export const AuthContext = createContext()
-
-export const userAuth = () => {
-    const context = useContext(AuthContext)
+export const useAuth = () => {
+    const context = useContext(AuthContext);
     if (!context) {
-        throw new Error("userAuth must be used within an AuthProvider")
+        throw new Error("useAuth must be used within an AuthProvider");
     }
     return context;
-}  
+};
 
-export const AuthProvider = ({children}) => {
-    const [user, setUser]=useState(null)
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [errors, setErrors] = useState([]); 
+    const [errors, setErrors] = useState([]);
 
-    const signup = async (user) =>{
+    const signup = async (user) => {
         try {
             const res = await registerRequest(user);
-            console.log(res.data);
             setUser(res.data);
             setIsAuthenticated(true);
         } catch (error) {
             setErrors(error.response.data);
         }
-    }
+    };
+
+    const signin = async (userData) => {
+        try {
+            const res = await loginRequest(userData);
+            setUser(res.data);
+            setIsAuthenticated(true);
+            setErrors([]);
+        } catch (error) {
+            if (Array.isArray(error.response.data)) {
+                setErrors(error.response.data);
+            } else {
+                setErrors([error.response.data.message || "Login failed"]);
+            }
+        }
+    };
 
     return (
-        <AuthContext.Provider value={{
-            signup,
-            user,
-            isAuthenticated,
-            errors,
-        }}>
+        <AuthContext.Provider
+            value={{
+                signup,
+                signin,
+                user,
+                isAuthenticated,
+                errors,
+            }}
+        >
             {children}
         </AuthContext.Provider>
-    )
-}
+    );
+};
